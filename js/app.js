94eaefbdb025b42a8e98adc45d61210318d68e2e
James Goatcher <jamesgoatcher@gmail.com>
2016-05-28 22:46:09 -0700

    Reattached js file, but not working as desired. WIP that is TBC.

--- js/app.js
+++ js/app.js
@@ -1,44 +1,89 @@
-//Start Photo Gallery
+//Start links to gallery classes
 window.onload = function() {
-	console.log('loaded');
 
-	var photoGalleryStart = document.getElementById('photo-gallery');
-	photoGalleryStart.onclick = function() {
-		startPhoto();
+	var homeStart = document.querySelector('#logo');
+	homeStart.onclick = function() {
+		startHome();
+	}
+
+	var theWorldStart = document.querySelector('#the_world');
+	theWorldStart.onclick = function() {
+		startWorld();
 	}
 }
 
+//Arrays
+var arrayPhotos = [
+'world-bryggen.jpg', 'world-gamcheon.jpg', 'world-morro.jpg', 'world-rocks.jpg', 'world-tokyo.jpg'
+];
+
+///////////
+//home logo
+///////////
+//Attaches the user's click to home logo
+function startHome() {
+	this.makeHome();
+}
+
+//Returns to landing page
+function makeHome() {
+	var resetBgHome = document.querySelector('.jumbo-gallery');
+	resetBgHome.className = 'jumbo';
+}
+
+///////////
+//the world
+///////////
 //Attaches the user's click to the Photo Gallery trigger
-function startPhoto() {
-	console.log('start photo gallery')
-	this.makePhotoGallery();
+function startWorld() {
+	this.makeWorldGallery();
+	this.makeUlDiv();
+	this.makeWorldUlandLi();
 }
 
-
-//Photo Gallery div array
-var photoDivArray = ['1', '2', '3', '4'];
 //Makes the Photo Gallery
-function makePhotoGallery() {
+function makeWorldGallery() {
+	var resetBgGallery = document.querySelector('.jumbo');
+	resetBgGallery.className = 'jumbo-gallery';
 
-	var jumboPhoto = document.querySelector('#jumbo-photo')
-	jumboPhoto.innerHTML = '';
-	console.log('cleared');
+	var jumboGalleryContainer = document.createElement('div');
+		jumboGalleryContainer.setAttribute('class', 'world-class');
+		resetBgGallery.appendChild(jumboGalleryContainer);
+}
 
-	for (var i = 0; i < photoDivArray.length; i++) {
-		var divCreate = document.createElement('div');
-		divCreate.setAttribute('class', 'photo-class');
-		divCreate.setAttribute('data-value', photoDivArray[i]);
-		jumboPhoto.appendChild(divCreate);
-		divCreate.addEventListener('click', function(event) {
-			var clickedHere = this;
-			openPhoto(clickedHere);
+//REUSABLE: Makes UL Div for World
+function makeUlDiv() {
+	var findWorldClass = document.querySelector('.world-class');
 
-		})
+	var ulDiv = document.createElement('div');
+		ulDiv.setAttribute('class', 'ul-div');
 
+		findWorldClass.appendChild(ulDiv);
+}
+
+//Creates the World ul and li loop with data-name to image
+function makeWorldUlandLi() {
+	var findUL = document.querySelector('.ul-div');
+	var createUL = document.createElement('ul');
+	
+	createUL.setAttribute('class', 'img');
+	findUL.appendChild(createUL);
+
+	for (i = 0; i < arrayPhotos.length; i++) {
+		var worldLi = document.createElement('li');
+		var worldLiGet = document.getElementsByClassName(arrayPhotos[i]);
+		var findImg = document.querySelector('.img');
+
+		worldLi.className = arrayPhotos[i];
+		worldLi.setAttribute('data-world', arrayPhotos[i]);
+
+		findImg.appendChild(worldLi);
+		console.log(worldLiGet[i]);
+		//worldLiGet[i].innerHTML = '<img src="img/'+arrayPhotos[i]+'">';
 	}
-	console.log('created divs')
 }
 
-function openPhoto(photo) {
-	console.log('gonna open the photo soon')
-}
+
+
+
+
